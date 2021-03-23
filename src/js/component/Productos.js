46f109f8a427e4511de1bfd0eadf26b1baf02849
Gilbert Marín <daviddlioncourt@gmail.com>
2021-03-23 19:24:32 +0000

    productos agregados

--- src/js/component/Productos.js
+++ src/js/component/Productos.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
+	const [list, setList] = useState([]);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	const getData = async () => {
+		const data = await fetch("https://fakestoreapi.herokuapp.com/products");
+		const prods = await data.json();
+		setList(prods);
+	};
+
 	return (
 		<>
 			<h1>Productos</h1>
+			<ul>
+				{list.map(item => (
+					<li key={item.id}>{item.title} </li>
+				))}
+			</ul>
 		</>
 	);
 };
